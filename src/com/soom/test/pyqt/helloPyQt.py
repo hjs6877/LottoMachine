c868f9791d37b0914eb4006ab90b557e22a9d90b
jshwang <jshwang@pentasecurity.com>
2016-02-24 10:33:45 +0900

    Hello PyQt5 작성 완료..

--- src/com/soom/test/pyqt/helloPyQt.py
+++ src/com/soom/test/pyqt/helloPyQt.py
@@ -1,1 +1,16 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+# from PyQt5.QtCore import *
+# from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(550, 350)
+    w.move(300, 300)
+    w.setWindowTitle('Hello World')
+    w.setToolTip("Hello PyQt5!!")
+    w.show()
+
+    sys.exit(app.exec_())
