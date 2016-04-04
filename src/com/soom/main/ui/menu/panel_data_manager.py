f22ae6f6d3d06c3b1997586c6c3b01df78dc11f7
hjs6877 <hjs6877@gmail.com>
2016-04-04 22:32:06 +0900

    유효성 검사 작업 중.

--- src/com/soom/main/ui/menu/panel_data_manager.py
+++ src/com/soom/main/ui/menu/panel_data_manager.py
@@ -45,25 +45,26 @@ class DataManager(wx.Panel):
         inital_value = "select number"
 
         # 번호 입력 ComboBox
-        combobox_number_first = wx.ComboBox(panel_number_combobox, id=0, value=inital_value, choices=number_list, size=wx.Size(100, 30))
-        combobox_number_second = wx.ComboBox(panel_number_combobox, id=1, value=inital_value, choices=number_list, size=wx.Size(100, 30))
-        combobox_number_third = wx.ComboBox(panel_number_combobox, id=2, value=inital_value, choices=number_list, size=wx.Size(100, 30))
-        combobox_number_fourth = wx.ComboBox(panel_number_combobox, id=3, value=inital_value, choices=number_list, size=wx.Size(100, 30))
-        combobox_number_fifth = wx.ComboBox(panel_number_combobox, id=4, value=inital_value, choices=number_list, size=wx.Size(100, 30))
-        combobox_number_sixth = wx.ComboBox(panel_number_combobox, id=5, value=inital_value, choices=number_list, size=wx.Size(100, 30))
+        self.combobox_number_first = wx.ComboBox(panel_number_combobox, id=0, value=inital_value, choices=number_list, size=wx.Size(100, 30))
+        self.combobox_number_second = wx.ComboBox(panel_number_combobox, id=1, value=inital_value, choices=number_list, size=wx.Size(100, 30))
+        self.combobox_number_third = wx.ComboBox(panel_number_combobox, id=2, value=inital_value, choices=number_list, size=wx.Size(100, 30))
+        self.combobox_number_fourth = wx.ComboBox(panel_number_combobox, id=3, value=inital_value, choices=number_list, size=wx.Size(100, 30))
+        self.combobox_number_fifth = wx.ComboBox(panel_number_combobox, id=4, value=inital_value, choices=number_list, size=wx.Size(100, 30))
+        self.combobox_number_sixth = wx.ComboBox(panel_number_combobox, id=5, value=inital_value, choices=number_list, size=wx.Size(100, 30))
 
         # 번호 저장 버튼
         button_save_number = wx.Button(panel_number_combobox, id=0, label="저장")
 
         # 번호 입력 ComboBox sizer에 ComboBox 추가
-        panel_number_combobox_sizer.Add(combobox_number_first, 0, wx.ALIGN_LEFT | wx.ALL, 10)
-        panel_number_combobox_sizer.Add(combobox_number_second, 0, wx.ALIGN_LEFT | wx.ALL, 10)
-        panel_number_combobox_sizer.Add(combobox_number_third, 0, wx.ALIGN_LEFT | wx.ALL, 10)
-        panel_number_combobox_sizer.Add(combobox_number_fourth, 0, wx.ALIGN_LEFT | wx.ALL, 10)
-        panel_number_combobox_sizer.Add(combobox_number_fifth, 0, wx.ALIGN_LEFT | wx.ALL, 10)
-        panel_number_combobox_sizer.Add(combobox_number_sixth, 0, wx.ALIGN_LEFT | wx.ALL, 10)
+        panel_number_combobox_sizer.Add(self.combobox_number_first, 0, wx.ALIGN_LEFT | wx.ALL, 10)
+        panel_number_combobox_sizer.Add(self.combobox_number_second, 0, wx.ALIGN_LEFT | wx.ALL, 10)
+        panel_number_combobox_sizer.Add(self.combobox_number_third, 0, wx.ALIGN_LEFT | wx.ALL, 10)
+        panel_number_combobox_sizer.Add(self.combobox_number_fourth, 0, wx.ALIGN_LEFT | wx.ALL, 10)
+        panel_number_combobox_sizer.Add(self.combobox_number_fifth, 0, wx.ALIGN_LEFT | wx.ALL, 10)
+        panel_number_combobox_sizer.Add(self.combobox_number_sixth, 0, wx.ALIGN_LEFT | wx.ALL, 10)
         panel_number_combobox_sizer.Add(button_save_number, 0, wx.ALIGN_LEFT | wx.ALL, 10)
 
+        # 이벤트 바인딩
         button_save_number.Bind(wx.EVT_BUTTON, self.save_number)
 
         # 데이터 관리 sizer에 번호 타입 패널을 추가
@@ -72,4 +73,30 @@ class DataManager(wx.Panel):
         panel_data_manager_sizer.Add(panel_number_combobox, 0, wx.ALIGN_LEFT | wx.ALL, 15)
 
     def save_number(self, event):
-        print("번호 저장..")
+        number1 = self.combobox_number_first.GetValue()
+        number2 = self.combobox_number_second.GetValue()
+        number3 = self.combobox_number_third.GetValue()
+        number4 = self.combobox_number_fourth.GetValue()
+        number5 = self.combobox_number_fifth.GetValue()
+        number6 = self.combobox_number_sixth.GetValue()
+        number_list = [number1, number2, number3, number4, number5, number6]
+
+        result = self.validate_number(number_list)
+
+        if result:
+            print("번호 저장..")
+
+    def validate_number(self, number_list):
+        num_dic = {}
+        for number in number_list:
+            if number == 'select number':
+                wx.MessageBox("선택하지 않은 번호가 있습니다.", "알림", wx.OK | wx.ICON_EXCLAMATION)
+                return False
+            if num_dic.get(number):
+                wx.MessageBox("중복된 번호가 존재합니다.", "알림", wx.OK | wx.ICON_EXCLAMATION)
+                return False
+            else:
+                num_dic[number] = True
+        return True
+
+
